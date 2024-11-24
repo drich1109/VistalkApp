f17eaced97bea75482740b6397358e13705bc7a7
Drich <93727360+drich1109@users.noreply.github.com>
2024-11-24 08:53:56 +0800

    Update payment.py

--- payment.py
+++ payment.py
@@ -11,7 +11,6 @@ def paymongoredirect():
         amount = data.get('amount')
         description = data.get('description')
         amount_in_cents = int(float(amount) * 100)
-        print(amount_in_cents)
         url = "https://api.paymongo.com/v1/links"
 
         payload = { "data": { "attributes": {
@@ -45,36 +44,56 @@ def handle_webhook():
     try:
 
         data = request.json
-        print(data)
+        description = data['data']['attributes']['data']['attributes']['description']
         event_type = data['data']['attributes']['type']
-        
-        if event_type == "payment.paid":
-            user_email = data['data']['attributes']['data']['attributes']['billing']['email']
-            
-            connection = get_db_connection()
-            cursor = connection.cursor()
-            print(user_email)
-
-            sql_query = """SELECT userID FROM user WHERE email = %s"""
-            cursor.execute(sql_query, (user_email,))
-            user_id = cursor.fetchone()
-            print(user_id)
-            if user_id:
-                user_id = user_id[0]  
+        user_email = data['data']['attributes']['data']['attributes']['billing']['email']
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        if 'Subscription' in description:
+            if event_type == "payment.paid":                
+                sql_query = """SELECT userID FROM user WHERE email = %s"""
+                cursor.execute(sql_query, (user_email,))
+                user_id = cursor.fetchone()
                 print(user_id)
+                if user_id:
+                    user_id = user_id[0]  
+                    print(user_id)
 
-                update_query = """
-                    UPDATE vista
-                    SET isPremium = 1, premiumDate = %s
-                    WHERE userPlayerId = %s
-                """
-                cursor.execute(update_query, (datetime.now(), user_id))
-                connection.commit()
+                    update_query = """
+                        UPDATE vista
+                        SET isPremium = 1, premiumDate = %s
+                        WHERE userPlayerId = %s
+                    """
+                    cursor.execute(update_query, (datetime.now(), user_id))
+                    connection.commit()
+                
+                return jsonify({"message": "Webhook handled successfully"}), 200
+            else:
+                return jsonify({"message": "Event not handled"}), 200
+        elif 'Bag' in description:
+            if event_type == "payment.paid":
+                coinBag_query = """SELECT quantity from coinbag where coinBagName = %s"""
+                cursor.execute(coinBag_query, (description,))
+                coinBag = cursor.fetchone()
+                sql_query = """SELECT userID FROM user WHERE email = %s"""
+                cursor.execute(sql_query, (user_email,))
+                user_id = cursor.fetchone()
+                if user_id:
+                    user_id = user_id[0]  
+                    print(user_id)
+
+                    update_query = """
+                        UPDATE vista
+                        SET vCoin = vCoin + %s
+                        WHERE userPlayerId = %s
+                    """
+                    cursor.execute(update_query, (coinBag[0], user_id))
+                    connection.commit()
+                
+                return jsonify({"message": "Webhook handled successfully"}), 200
+            else:
+                return jsonify({"message": "Event not handled"}), 200
             
-            return jsonify({"message": "Webhook handled successfully"}), 200
-        else:
-            return jsonify({"message": "Event not handled"}), 200
-
     except Exception as e:
         print(f"Error: {str(e)}")
         return jsonify({"error": "An error occurred"}), 500
@@ -88,13 +107,13 @@ def poolSubscription():
 
         
     result = cursor.fetchone()
-    print(result)     
+
+        
     if result:
         is_premium, premium_date = result
-        today_date = datetime.now().date()
+
             
-        if is_premium and premium_date and premium_date == today_date:
-            print(True)
+        if is_premium and premium_date and premium_date >= datetime.now():
             return jsonify({
             'isSuccess': True,
             'message': 'Subscribed',
@@ -103,10 +122,36 @@ def poolSubscription():
             'totalCount': 0
         }), 200
         else:
-            print(False)
             return jsonify({
             'isSuccess': True,
             'message': 'Not Subscribed',
+            'data': False,
+            'data2': None,
+            'totalCount': 0
+        }), 200  
+            
+def poolCoinBag():
+    userId = request.args.get('userId')
+    vcoin = request.args.get('vCoin')
+    vcoin = int(vcoin)
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    sql_query = """SELECT vCoin FROM vista WHERE userPlayerId = %s"""
+    cursor.execute(sql_query, (userId,))
+    result = cursor.fetchone()     
+    if result:            
+        if result[0] > vcoin:
+            return jsonify({
+                'isSuccess': True,
+                'message': 'Vcoin Purchased',
+                'data': True,
+                'data2': None,
+                'totalCount': 0
+            }), 200
+        else:
+            return jsonify({
+            'isSuccess': True,
+            'message': 'Not yet Purchased',
             'data': False,
             'data2': None,
             'totalCount': 0
